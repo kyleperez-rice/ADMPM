aceeb8b15f18d04324fd4528bc7d58a26348fc79
kyleperez-rice <69982842+kyleperez-rice@users.noreply.github.com>
2021-04-23 11:57:48 -0600

    Delete csrc/elastic_solid directory
    Reformed c code to be better.

--- csrc/elastic_solid/solver_options.c
+++
@@ -1,6 +0,0 @@
-#include "solver_options.h"
-
-// Somehow how we specify these dudes in an external file
-char* node_titles[nd_titles_size] = {"time", "xpos", "stress", "strain", "dens", "xvel"};
-
-char* mp_titles[ mp_titles_size ] = {"time", "xpos", "mass", "strain", "stress", "xvel"};
